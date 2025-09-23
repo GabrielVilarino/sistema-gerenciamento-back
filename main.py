3f2e107d5896aa06f082b02fbcc9006db1f668e1
GabrielVilarino <gabriel_vilarino@hotmail.com>
2025-09-23 12:44:41 -0300

    [FIX] importando env primeiro

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
-from src.routes import login, users, cargos, produtos, categorias, vendas
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from fastapi import FastAPI
+from src.routes import login, users, cargos, produtos, categorias, vendas
+
 
 app = FastAPI()
 
